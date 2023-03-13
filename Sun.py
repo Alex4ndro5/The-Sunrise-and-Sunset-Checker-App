8dd3306e7d4d50b6a1a1a680a64b34722a705647
Alex4ndro5 <81052341+Alex4ndro5@users.noreply.github.com>
2023-03-13 21:29:03 +0100

    MainFile

---
+++ Sun.py
@@ -0,0 +1,32 @@
+import requests
+from datetime import datetime
+ 
+def convert24(time):
+    # Parse the time string into a datetime object
+    t = datetime.strptime(time, '%I:%M:%S %p')
+    # Format the datetime object into a 24-hour time string
+    return t.strftime('%H:%M:%S')
+
+#Api zdobywajace lokacje
+location = input("Podaj miejsce:")
+getLocation = 'https://nominatim.openstreetmap.org/?addressdetails=1&q={}&format=json&limit=1'
+
+response = requests.get(getLocation.format(location)).json()
+
+lat = response[0]['lat']
+lon = response[0]['lon']
+#Api uzyskujace dane o sloncu na podstawie lokacji
+getSun = 'https://api.sunrise-sunset.org/json?lat={}&lng={}'
+
+result = requests.get(getSun.format(lat, lon)).json()
+
+#Api do pogody
+getWet = 'https://api.open-meteo.com/v1/forecast?latitude={}&longitude={}&current_weather=true'
+info = requests.get(getWet.format(lat, lon)).json()
+temp = info['current_weather']['temperature']
+wind = info['current_weather']['windspeed']
+#Konwersja na czas 24h
+srise = convert24(result['results']['sunrise'])
+sset = convert24(result['results']['sunset'])
+#Wynik
+print("Wschód słońca w %s o %s, a zachód o %s.\nTemperatura wynosi %d stopni Celsjusza, a prędkość wiatru %d km/h."%(location, srise, sset, temp, wind))
